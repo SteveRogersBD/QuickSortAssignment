5378f2a5f98f54d77b1576d128cf1a39a6ef4c47
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-10-03 15:14:06 -0500

    edgecase added

--- src/main/java/org/example/QuickSort.java
+++ src/main/java/org/example/QuickSort.java
@@ -100,5 +100,29 @@ public class QuickSort {
         quickSort(arr,0,arr.length-1);
         System.out.println("Sorted:");
         printArray(arr);
+
+
+        //edgecase1
+        System.out.println();
+        System.out.println("Test case: Large array");
+        arr = new int[] {710, -576, 645, 505, 143, -383, 906, -899, -451, 943, -278, -427, 121, 585, -466, 868, 568,
+                -145, 810, 378, 939, -621, -409, -653, 714, -889, -39, -591, -144, 489, -365, 426, -683, -439,
+                -20, 438, 439, 291, -254, 134, 482, 753, -721, -97, 38, -695, -964, -568, -443, 673, 911, -94,
+                -376, 715, 83, 761, -95, -403, -946, -441, -292, -730, 423, -57, 979, -379, -237, -44, -858,
+                -589, 780, 302, 126, 477, 388, -90, -732, -282, -41, 198, -872, 361, -477, 478, -225, 91, -317,
+                -258, 125, 821, -462, -695, -837, 404, -294, -705, 970, -334, -532, 569}
+        ;
+        quickSort(arr,0,arr.length-1);
+        System.out.println("Sorted:");
+        printArray(arr);
+
+        //edgecae2
+        System.out.println();
+        System.out.println("Test case: Duplicate elements");
+        arr = new int[] {5, 5, 5, 10, 10, 10, 1, 1, 1, 7, 7, 7, 2, 2, 2, 5, 5, 5, 10, 10};
+        quickSort(arr,0,arr.length-1);
+        System.out.println("Sorted:");
+        printArray(arr);
+
     }
 }
